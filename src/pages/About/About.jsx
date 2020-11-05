316ab40a300740b6cb5b4e0f7165886bb011f0c4
kaleginiene <ieva.kaleginiene@gmail.com>
2020-11-05 22:11:51 +0200

    Aded about lorem ipsum article

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,34 @@ import * as S from "./About.style";
 function About() {
   return (
     <S.About>
-      <Section fullWidth>About page</Section>
+      <Section>
+        <h2>About us</h2>
+        <article>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit beatae
+          ipsam alias voluptatum, debitis totam cum, error optio modi
+          dignissimos, aliquid autem porro voluptatibus quam atque voluptas
+          ducimus saepe blanditiis? Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Expedita nisi perferendis ex modi nesciunt corporis
+          maxime, tempora velit sint et, cumque veritatis, dolor numquam alias
+          autem aliquid odio animi eum. Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. A, fugiat? Minima quos quidem mollitia et ipsa neque
+          tempore quasi officia ipsam minus! Reprehenderit omnis iusto labore,
+          non rerum sit explicabo?
+        </article>
+        <br />
+        <article>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit beatae
+          ipsam alias voluptatum, debitis totam cum, error optio modi
+          dignissimos, aliquid autem porro voluptatibus quam atque voluptas
+          ducimus saepe blanditiis? Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Expedita nisi perferendis ex modi nesciunt corporis
+          maxime, tempora velit sint et, cumque veritatis, dolor numquam alias
+          autem aliquid odio animi eum. Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. A, fugiat? Minima quos quidem mollitia et ipsa neque
+          tempore quasi officia ipsam minus! Reprehenderit omnis iusto labore,
+          non rerum sit explicabo?
+        </article>
+      </Section>
     </S.About>
   );
 }
