4cb68f7aafd0235e74fb1e44d2ad97ac962dcb2b
kaleginiene <ieva.kaleginiene@gmail.com>
2020-11-10 19:16:28 +0200

    createOrder function in productSelection page

--- src/pages/ProductSelection/ProductSelection.jsx
+++ src/pages/ProductSelection/ProductSelection.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Section } from "../../components";
 import * as S from "./ProductSelection.style";
 import products from "../../utils/products";
+import { UserContext } from "../../context/user.context";
+import { useHistory } from "react-router-dom";
+
+function createOrder(userID, item) {
+  fetch("http://localhost:8080/order", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userID, item }),
+  });
+}
 
 function ProductSelection() {
+  const user = React.useContext(UserContext);
+  let userID = user.state;
+  userID = userID.data.id;
+
+  const [item, setItem] = useState("");
+  const history = useHistory();
+
   return (
     <S.ProductSelection>
       <Section>
@@ -14,7 +33,11 @@ function ProductSelection() {
               <Card
                 title={product.title}
                 image={product.image}
-                handleClick={() => alert(product.title)}
+                handleClick={() => {
+                  setItem(product.title);
+                  createOrder(userID, item);
+                  history.push("/confirm");
+                }}
               />
             </S.CardWrapper>
           ))}
